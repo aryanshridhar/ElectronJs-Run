deef2842db6667e7f96e8fb25c16fd294be7243b
aryanshridhar <aryanshridhar7@gmail.com>
2020-07-29 17:27:28 +0530

    Starting dev

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 650,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    resizable : false,
+    fullscreen : false
+  })
+
+  win.loadFile('./screen/index.html')
+
+
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
